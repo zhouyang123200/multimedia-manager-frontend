fb10a222ee940dbfdebf6fee8f95cdff56a27f8b
zhouyang <zhouyang@hotmail.com>
2019-12-27 23:12:50 +0800

    add post video entry api

--- src/app/video-uploader/video-uploader.component.ts
+++ src/app/video-uploader/video-uploader.component.ts
@@ -3,7 +3,14 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 import { HttpClient, HttpRequest, HttpEvent, HttpEventType } from '@angular/common/http';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FileUploadService } from '../services/file-upload.service'
-import { map, tap, catchError } from 'rxjs/operators';
+import { map, tap, catchError, timestamp, last } from 'rxjs/operators';
+
+interface FileUploadInfo {
+  fileObj: File,
+  percent: number,
+  uploaded: boolean,
+  timestamp: string
+}
 
 @Component({
   selector: 'app-video-uploader',
@@ -15,18 +22,23 @@ export class VideoUploaderComponent implements OnInit {
   closeResult: string;
   @Input()id: number;
   myForm: FormGroup;
-  videoFile: File;
-  imageFile: File;
-  videoUploadPercent: number = 0;
-  imageUploadPercent: number = 0;
-  vieoTimestamp: string;
-  imageTimestamp: string;
+  videoUrl: string = 'http://localhost:4200/api/videos';
+  videoUploadInfo: FileUploadInfo = {
+    fileObj: null,
+    percent: 0,
+    uploaded: false,
+    timestamp: ''
+  };
+  imageUploadInfo: FileUploadInfo = {
+    fileObj: null,
+    percent: 0,
+    uploaded: false,
+    timestamp: ''
+  };
 
   // states
   imageSelected: boolean = false;
   videoSelected: boolean = false;
-  imageUploaded: boolean = false;
-  videoUploaded: boolean = false;
 
 
   constructor(public activeModal: NgbActiveModal,
@@ -42,13 +54,25 @@ export class VideoUploaderComponent implements OnInit {
 
   private createForm() {
     this.myForm = this.formBuilder.group({
-      username: '',
-      password: '',
+      title: '',
+      description: '',
     });
   }
 
   private submitForm() {
     console.log(this.myForm.value.password);
+    // send form request
+    const data = {
+      'title': this.myForm.value.title,
+      'description': this.myForm.value.description,
+      'video_name': this.videoUploadInfo.fileObj.name,
+      'video_num': this.videoUploadInfo.timestamp,
+      'image_name': this.imageUploadInfo.fileObj.name,
+      'image_num': this.imageUploadInfo.timestamp
+    }
+    this.http.post(this.videoUrl, data).subscribe((rep) => {
+      console.log(rep);
+    })
     this.activeModal.close(this.myForm.value);
   }
 
@@ -59,45 +83,55 @@ export class VideoUploaderComponent implements OnInit {
   videoFileChange(files: [File]) {
     if (files && files[0]) {
       this.videoSelected = true;
-      this.videoFile = files[0];
+      this.videoUploadInfo.fileObj = files[0];
     } else {
       this.videoSelected = false;
-      this.videoFile = null;
+      this.videoUploadInfo.fileObj = null;
     }
   }
 
   imageFileChange(files: [File]) {
     if (files && files[0]) {
       this.imageSelected = true;
-      this.imageFile = files[0];
+      this.imageUploadInfo.fileObj = files[0];
     } else {
       this.imageSelected = false;
-      this.imageFile = null;
+      this.imageUploadInfo.fileObj = null; 
     }
   }
 
   uploadAllFiles(): void {
-    if (this.videoFile) {
-      this.fileUploader.upload(this.videoFile,
+    if (this.videoUploadInfo.fileObj) {
+      this.fileUploader.upload(this.videoUploadInfo.fileObj,
          'http://localhost:4200/api/files').pipe(
-            map(evt => this.uploadEventMsg(evt, this.videoFile)),
-            tap(msg => console.log(msg))
-         ).subscribe();
+            map(evt => this.uploadEventMsg(evt, this.videoUploadInfo)),
+            tap(msg => console.log(msg)),
+            last()
+         ).subscribe(ret => this.videoUploadInfo.timestamp = ret.timestamp);
     }  
+    if (this.imageUploadInfo.fileObj) {
+      this.fileUploader.upload(this.imageUploadInfo.fileObj,
+        'http://localhost:4200/api/files').pipe(
+          map(evt => this.uploadEventMsg(evt, this.imageUploadInfo)),
+          tap(msg => console.log(msg)),
+          last()
+        ).subscribe(ret => this.imageUploadInfo.timestamp = ret.timestamp);
+    }
   }
 
-  private uploadEventMsg(event: HttpEvent<any>, file: File) {
+  private uploadEventMsg(event: HttpEvent<any>, fileInfo: FileUploadInfo) {
     switch(event.type) {
       case HttpEventType.Sent:
-        return `Uploading file: ${file.name} ${file.size}`;
+        return `Uploading file: ${fileInfo.fileObj.name} ${fileInfo.fileObj.size}`;
       case HttpEventType.UploadProgress:
-        this.videoUploadPercent = Math.round(100 * event.loaded / event.total);
-        return `File "${file.name}" is ${this.videoUploadPercent }% uploaded.`;
+        fileInfo.percent = Math.round(100 * event.loaded / event.total);
+        return `File "${fileInfo.fileObj.name}" is ${fileInfo.percent}% uploaded.`;
       case HttpEventType.Response:
-        this.videoUploaded = true;
-        return `File "${file.name}" was completely uploaded!`;
+        fileInfo.uploaded = true;
+        console.log(`File "${fileInfo.fileObj.name}" was completely uploaded!`);
+        return event.body;
       default:
-        return `File "${file.name}" surprising upload event: ${event.type}.`;
+        return `File "${fileInfo.fileObj.name}" surprising upload event: ${event.type}.`;
     }
   }
 }

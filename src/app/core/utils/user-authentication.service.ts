72c16f94376d6d6ea876abe761d01f7406c9a706
zhou yang <zhouyang123200@hotmail.com>
2021-04-11 19:42:14 +0800

    add login method in authservice

--- src/app/core/utils/user-authentication.service.ts
+++ src/app/core/utils/user-authentication.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { BrowserStorageService } from './browser-storage.service';
-import { Observable, BehaviorSubject } from 'rxjs';
-import { tap, map, catchError } from 'rxjs/operators'
-import { sign } from 'fake-jwt-sign'
+import { Observable, BehaviorSubject, of, throwError } from 'rxjs';
+import { tap, map, catchError } from 'rxjs/operators';
+import { sign } from 'fake-jwt-sign';
+import { environment } from '../../../environments/environment';
 
 export interface IAuthStatus {
   isAuthenticated: boolean
@@ -24,6 +25,10 @@ export interface IServerAuthResponse {
 export abstract class AuthService implements IAuthService {
   authStatus$: BehaviorSubject<IAuthStatus>;
 
+  constructor(
+    protected browserStorageService: BrowserStorageService,
+  ) {}
+
   login(username: string, passwd: string): Observable<void> {
     throw new Error('Method not implemented.');
   }
@@ -33,7 +38,15 @@ export abstract class AuthService implements IAuthService {
   }
 
   getToken(): string {
-    throw new Error('Method not implemented.');
+    return this.browserStorageService.get('jwt') ?? ''
+  }
+
+  protected setToken(jwt: string) {
+    this.browserStorageService.set('jwt', jwt);
+  }
+
+  protected clearToken() {
+    this.browserStorageService.remove('jwt');
   }
 
   protected abstract authProvider(username:string, passwd: string): Observable<IServerAuthResponse>;
@@ -48,39 +61,51 @@ export class InMemoryAuthService extends AuthService implements IAuthService {
   authStatus$: BehaviorSubject<IAuthStatus>;
 
   constructor(
-    private browserStorageService: BrowserStorageService,
+    protected browserStorageService: BrowserStorageService,
     private http: HttpClient
     ) {
-      super();
+      super(browserStorageService);
       console.warn(
         "You're using the InMemoryAuthService. Do not use this service in production."
       )
     }
 
   protected authProvider(username: string, passwd: string): Observable<IServerAuthResponse> {
-    throw new Error('Method not implemented.');
+    const authStatus = {
+      isAuthenticated: true,
+      userId: 1
+    } as unknown as IAuthStatus;
+    const authResponse = {
+      accessToken: sign(authStatus, 'secret', {
+        expiresIn: '1h',
+        algorithm: 'none'
+      })
+    } as IServerAuthResponse;
+    return of(authResponse);
   }
 
   getToken(): string {
     throw new Error('Method not implemented.');
   }
 
-  checkLogin():boolean {
-    let accsess_token = this.browserStorageService.get('accsess_token');
-    return accsess_token !== null;
-  }
-
   login(username: string, passwd: string): Observable<any> {
     const userInfo = {
       username,
       passwd
-    }
-    return this.http.post<{access_token: string}>(this.loginUrl, userInfo).pipe(
-      map(tokenObj => tokenObj.access_token),
+    };
+    const logResponse$ =  this.authProvider(username, passwd).pipe(
+      map(tokenObj => tokenObj.accessToken),
       tap(token => {
         this.browserStorageService.set('access_token', token);
       })
     );
+    logResponse$.subscribe({
+      error: (error) => {
+        this.logout();
+        return throwError(error);
+      }
+    })
+    return logResponse$;
   }
 
   logout(clearToken?: boolean): void {
@@ -93,6 +118,13 @@ export class InMemoryAuthService extends AuthService implements IAuthService {
 })
 export class UserAuthenticationService extends AuthService implements IAuthService {
   authStatus$: BehaviorSubject<IAuthStatus>;
+
+  constructor(
+    protected browserStorageService: BrowserStorageService,
+    private http: HttpClient
+    ) {
+      super(browserStorageService);
+    }
 
   login(username: string, passwd: string): Observable<void> {
     throw new Error('Method not implemented.');
@@ -107,7 +139,10 @@ export class UserAuthenticationService extends AuthService implements IAuthServi
   }
 
   protected authProvider(username: string, passwd: string): Observable<IServerAuthResponse> {
-    throw new Error('Method not implemented.');
+    return this.http.post<IServerAuthResponse>(`${environment.baseUrl}/api/authtoken`, {
+      username: username,
+      passwd: passwd
+    })
   }
 
 }

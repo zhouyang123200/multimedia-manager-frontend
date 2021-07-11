21669d9796224ee42001f4892620ee1fd5913287
zhou yang <zhouyang123200@hotmail.com>
2021-07-11 15:19:17 +0800

    change css to scss

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -6,7 +6,7 @@ import { AuthService } from "@utils/user-authentication.service";
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
   addressForm = this.fb.group({
